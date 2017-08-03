94064c3ad9a99be294d08e0a8d582bd7c31b96a8
Drabek Jan <drabek.honza@gmail.com>
2017-08-03 15:34:38 +0200

    + Alfonz MVVM dependencies

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/mainActivity/MainActivity.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/mainActivity/MainActivity.kt
@@ -2,10 +2,7 @@ package com.strvacademy.drabekj.moviestrv.mainActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.strvacademy.drabekj.moviestrv.MoviesApplication
 import com.strvacademy.drabekj.moviestrv.R
-import org.alfonz.utility.Logcat
 
 class MainActivity : AppCompatActivity() {
 
