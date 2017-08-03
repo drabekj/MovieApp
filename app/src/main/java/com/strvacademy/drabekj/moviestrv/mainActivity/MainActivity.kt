50c09382713f5e2a4406da2f81ab87b70a79ec75
Drabek Jan <drabek.honza@gmail.com>
2017-08-03 15:06:59 +0200

    + Alfonz logging

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/mainActivity/MainActivity.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/mainActivity/MainActivity.kt
@@ -2,7 +2,10 @@ package com.strvacademy.drabekj.moviestrv.mainActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.strvacademy.drabekj.moviestrv.MoviesApplication
 import com.strvacademy.drabekj.moviestrv.R
+import org.alfonz.utility.Logcat
 
 class MainActivity : AppCompatActivity() {
 
