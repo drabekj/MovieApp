141339d981348f20907827a6c4a32ab423ff25fa
Drabek Jan <drabek.honza@gmail.com>
2017-08-04 13:33:13 +0200

    + Convinience method to make Toast in Activities

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/utils/BaseActivity.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/utils/BaseActivity.kt
@@ -1,8 +1,12 @@
 package com.strvacademy.drabekj.moviestrv.utils
 
+import android.widget.Toast
+import com.strvacademy.drabekj.moviestrv.MoviesApplication
 import org.alfonz.mvvm.AlfonzActivity
 
 
 abstract class BaseActivity: AlfonzActivity() {
-
+    fun showToast(message: String) {
+        Toast.makeText(MoviesApplication.getContext(), message, Toast.LENGTH_SHORT).show()
+    }
 }
