1cc778667903af34f2a8d511f4ccfb8b74f7b694
Drabek Jan <drabek.honza@gmail.com>
2017-08-06 23:24:09 +0200

    + Show actor list

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/actorsActivity/ActorsFragment.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/actorsActivity/ActorsFragment.kt
@@ -1,11 +1,17 @@
 package com.strvacademy.drabekj.moviestrv.actorsActivity
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
+import com.strvacademy.drabekj.moviestrv.MoviesApplication
 import com.strvacademy.drabekj.moviestrv.databinding.FragmentActorsBinding
+import com.strvacademy.drabekj.moviestrv.model.Actor
 import com.strvacademy.drabekj.moviestrv.utils.BaseFragment
 
 
 class ActorsFragment: BaseFragment<ActorsView, ActorsViewModel, FragmentActorsBinding>(), ActorsView {
+	private var mAdapter: ActorsActivityAdapter? = null
+
 
 	override fun getViewModelClass(): Class<ActorsViewModel> {
 		return ActorsViewModel::class.java
@@ -15,7 +21,19 @@ class ActorsFragment: BaseFragment<ActorsView, ActorsViewModel, FragmentActorsBi
 		return FragmentActorsBinding.inflate(inflater!!)
 	}
 
-	override fun onActorClick(int: Int) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	override fun onActivityCreated(savedInstanceState: Bundle?) {
+		super.onActivityCreated(savedInstanceState)
+		setupAdapter()
+	}
+
+	override fun onActorClick(actor: Actor) {
+		Toast.makeText(MoviesApplication.getContext(), "Click " + actor.id, Toast.LENGTH_SHORT).show()
+	}
+
+	private fun setupAdapter() {
+		if (mAdapter == null) {
+			mAdapter = ActorsActivityAdapter(this, viewModel)
+			binding.fragmentActorsListRecycler.adapter = mAdapter
+		}
 	}
 }
--- app/src/main/java/com/strvacademy/drabekj/moviestrv/actorsActivity/ActorsView.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/actorsActivity/ActorsView.kt
@@ -1,8 +1,9 @@
 package com.strvacademy.drabekj.moviestrv.actorsActivity
 
+import com.strvacademy.drabekj.moviestrv.model.Actor
 import com.strvacademy.drabekj.moviestrv.utils.BaseView
 
 
 interface ActorsView: BaseView {
-	fun onActorClick(int: Int)
+	fun onActorClick(actor: Actor)
 }
