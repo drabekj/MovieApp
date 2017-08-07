1af3e4ec50ec3164ae5cd5cd4dc41e3e684d7f4a
Drabek Jan <drabek.honza@gmail.com>
2017-08-07 12:42:12 +0200

    + Basic ActorDetail design

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/model/Actor.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/model/Actor.kt
@@ -1,5 +1,5 @@
 package com.strvacademy.drabekj.moviestrv.model
 
 
-class Actor(var id: Int, var name: String, var birthDate: String, var moviesArray: Array<String>) {
+class Actor(var id: Int, var name: String, var birthDate: String, var description: String, var moviesArray: Array<String>) {
 }
