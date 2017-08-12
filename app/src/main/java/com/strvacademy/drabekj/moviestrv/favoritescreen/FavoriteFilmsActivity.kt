4350517c6fe3437e9670f60852e8b651ea36bdfd
Drabek Jan <drabek.honza@gmail.com>
2017-08-13 00:34:36 +0200

    + FavoriteFilms toolbar menu

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/favoritescreen/FavoriteFilmsActivity.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/favoritescreen/FavoriteFilmsActivity.kt
@@ -1,6 +1,9 @@
 package com.strvacademy.drabekj.moviestrv.favoritescreen
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.strvacademy.drabekj.moviestrv.R
 import com.strvacademy.drabekj.moviestrv.utils.BaseActivity
 
@@ -11,6 +14,27 @@ class FavoriteFilmsActivity: BaseActivity() {
 		setContentView(R.layout.activity_favorite_films)
 
 		setupToolbar()
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.favorite_films_toolbar_menu, menu)
+		return super.onCreateOptionsMenu(menu)
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+		when (item!!.itemId) {
+			R.id.action_edit -> {
+				showToast("Edit action")
+				return true
+			}
+
+			R.id.action_share -> {
+				showToast("Share action")
+				return true
+			}
+
+			else -> return super.onOptionsItemSelected(item)
+		}
 	}
 
 	private fun setupToolbar() {
