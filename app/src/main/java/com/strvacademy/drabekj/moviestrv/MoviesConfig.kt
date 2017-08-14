96226a01cf4f2b16822e28adb5e17a215f1f792e
Drabek Jan <drabek.honza@gmail.com>
2017-08-14 11:14:39 +0200

    * Refactored Config class into Object

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/MoviesConfig.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/MoviesConfig.kt
@@ -1,13 +1,11 @@
 package com.strvacademy.drabekj.moviestrv
 
 
-class MoviesConfig {
-    companion object {
-        const val LOGS = BuildConfig.LOGS
-        const val DEV_ENVIRONMENT = BuildConfig.DEV_ENVIRONMENT
+object MoviesConfig {
+	const val LOGS = BuildConfig.LOGS
+	const val DEV_ENVIRONMENT = BuildConfig.DEV_ENVIRONMENT
 
-//        const val REST_BASE_URL_PROD = "http://dev.markitondemand.com/MODApis/Api/v2/"
-//        const val REST_BASE_URL_DEV = "http://dev.markitondemand.com/MODApis/Api/v2/"
-//        const val REST_BASE_URL = if (MoviesConfig.DEV_ENVIRONMENT) MoviesConfig.REST_BASE_URL_DEV else MoviesConfig.REST_BASE_URL_PROD
-    }
+	//        const val REST_BASE_URL_PROD = "http://dev.markitondemand.com/MODApis/Api/v2/"
+	//        const val REST_BASE_URL_DEV = "http://dev.markitondemand.com/MODApis/Api/v2/"
+	//        const val REST_BASE_URL = if (MoviesConfig.DEV_ENVIRONMENT) MoviesConfig.REST_BASE_URL_DEV else MoviesConfig.REST_BASE_URL_PROD
 }
