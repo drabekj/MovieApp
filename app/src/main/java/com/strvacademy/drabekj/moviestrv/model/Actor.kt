6ef8e96697205bcaf3ab1961a1a8cb959802bce0
Drabek Jan <drabek.honza@gmail.com>
2017-08-14 11:49:29 +0200

    * Refactor model classes representing data objects made into 'data class'

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/model/Actor.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/model/Actor.kt
@@ -1,8 +1,5 @@
 package com.strvacademy.drabekj.moviestrv.model
 
 
-class Actor(var id: Int, var name: String, var birthDate: String, var description: String, var moviesArray: Array<String>) {
-
-
-
+data class Actor(var id: Int, var name: String, var birthDate: String, var description: String, var moviesArray: Array<String>) {
 }
