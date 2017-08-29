f6f863d03e22d908e5714f9897a28d1b6c4bfe79
Drabek Jan <drabek.honza@gmail.com>
2017-08-29 13:39:36 +0200

    - Remove redundant retrofit data loading call from MainActivity

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/ui/main/MainActivity.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/ui/main/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
 import com.strvacademy.drabekj.moviestrv.R
-import com.strvacademy.drabekj.moviestrv.model.MoviesDataResponse
-import com.strvacademy.drabekj.moviestrv.model.remote.TheMovieDbApiProvider
 import com.strvacademy.drabekj.moviestrv.utils.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import com.strvacademy.drabekj.moviestrv.ui.actors.ActorsFragment
 import com.strvacademy.drabekj.moviestrv.ui.movies.MoviesFragment
 import com.strvacademy.drabekj.moviestrv.ui.profile.ProfileFragment
-import org.alfonz.utility.Logcat
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class MainActivity : BaseActivity() {
@@ -25,8 +19,6 @@ class MainActivity : BaseActivity() {
 
 		setupBottomNavView()
 		showFragmentInTab(MoviesFragment.newInstance())
-
-		retrofitNew()
 	}
 
 	private fun setupBottomNavView() {
@@ -53,19 +45,5 @@ class MainActivity : BaseActivity() {
 		if (findViewById<View>(R.id.fragment_container) != null) {
 			supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
 		}
-	}
-
-	private fun retrofitNew() {
-		TheMovieDbApiProvider.newInstance()!!.getPopularMovies().enqueue(object : Callback<MoviesDataResponse> {
-			override fun onFailure(call: Call<MoviesDataResponse>?, t: Throwable?) {
-				showToast("Fail")
-				Logcat.d("retrofit fail |" + t.toString())
-			}
-
-			override fun onResponse(call: Call<MoviesDataResponse>?, response: Response<MoviesDataResponse>?) {
-				showToast("Success")
-				Logcat.d("retrofit successLoadingData: " + response!!.body())
-			}
-		})
 	}
 }
