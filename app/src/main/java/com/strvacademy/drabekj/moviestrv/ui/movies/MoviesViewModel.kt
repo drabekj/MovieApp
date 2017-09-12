6bfe63c82f77788a16f19ed7dc08755f8022ce0d
Drabek Jan <drabek.honza@gmail.com>
2017-09-12 12:57:49 +0200

    - Remove unnecessary state variable

--- app/src/main/java/com/strvacademy/drabekj/moviestrv/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/ui/movies/MoviesViewModel.kt
@@ -1,9 +1,7 @@
 package com.strvacademy.drabekj.moviestrv.ui.movies
 
-import android.databinding.ObservableField
 import com.strvacademy.drabekj.moviestrv.utils.BaseViewModel
 
 
 class MoviesViewModel : BaseViewModel<MoviesView>() {
-	val state = ObservableField<Int>()
 }
