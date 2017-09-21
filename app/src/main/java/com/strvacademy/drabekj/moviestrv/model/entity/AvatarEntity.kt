90f4489042902bf2feaf5927e374db5774cc79fa
Drabek Jan <drabek.honza@gmail.com>
2017-09-22 00:46:24 +0200

    + load profile image on Profile screen

---
+++ app/src/main/java/com/strvacademy/drabekj/moviestrv/model/entity/AvatarEntity.kt
@@ -0,0 +1,10 @@
+package com.strvacademy.drabekj.moviestrv.model.entity
+
+import com.google.gson.annotations.SerializedName
+
+class AvatarEntity {
+
+	@SerializedName("gravatar")
+	var gravatar: HashEntity? = null
+
+}
